aca382a36bb86b1e0b95efd9a02d8e6af87f217f
Arman <407448+armanist@users.noreply.github.com>
2026-04-30 20:36:44 +0400

    [#482] Add migration template unit tests

---
+++ tests/Unit/Migration/Templates/MigrationTemplateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Quantum\Tests\Unit\Migration\Templates;
+
+use Quantum\Migration\Templates\MigrationTemplate;
+use Quantum\Tests\Unit\AppTestCase;
+
+class MigrationTemplateTest extends AppTestCase
+{
+    public function testCreateTemplateContainsExpectedOperations(): void
+    {
+        $template = MigrationTemplate::create('create_table_users_1001', 'users');
+
+        $this->assertStringContainsString('class Create_table_users_1001 extends QtMigration', $template);
+        $this->assertStringContainsString("\$tableFactory->create('users')", $template);
+        $this->assertStringContainsString("\$tableFactory->drop('users')", $template);
+    }
+
+    public function testAlterTemplateContainsExpectedOperations(): void
+    {
+        $template = MigrationTemplate::alter('alter_table_users_1002', 'users');
+
+        $this->assertStringContainsString('class Alter_table_users_1002 extends QtMigration', $template);
+        $this->assertStringContainsString("\$tableFactory->get('users')", $template);
+    }
+
+    public function testRenameTemplateContainsExpectedOperations(): void
+    {
+        $template = MigrationTemplate::rename('rename_table_users_1003', 'users');
+
+        $this->assertStringContainsString('class Rename_table_users_1003 extends QtMigration', $template);
+        $this->assertStringContainsString("\$tableFactory->rename('users', \$newName)", $template);
+        $this->assertStringContainsString("\$tableFactory->rename(\$newName, 'users')", $template);
+    }
+
+    public function testDropTemplateContainsExpectedOperations(): void
+    {
+        $template = MigrationTemplate::drop('drop_table_users_1004', 'users');
+
+        $this->assertStringContainsString('class Drop_table_users_1004 extends QtMigration', $template);
+        $this->assertStringContainsString("\$tableFactory->drop('users')", $template);
+    }
+}
