d171b5f557b915c0a8d66d51951810bfba41f1fa
Arman <407448+armanist@users.noreply.github.com>
2026-05-01 21:07:25 +0400

    [#494] Refactor UploadedFile into upload collaborators

---
+++ src/Storage/Uploads/UploadStorage.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 3.0.0
+ */
+
+namespace Quantum\Storage\Uploads;
+
+use Quantum\Storage\Contracts\LocalFilesystemAdapterInterface;
+use Quantum\Storage\Contracts\FilesystemAdapterInterface;
+use Quantum\Storage\UploadedFile;
+
+class UploadStorage
+{
+    private LocalFilesystemAdapterInterface $localFileSystem;
+
+    public function __construct(LocalFilesystemAdapterInterface $localFileSystem)
+    {
+        $this->localFileSystem = $localFileSystem;
+    }
+
+    public function store(UploadedFile $file, string $targetPath, ?FilesystemAdapterInterface $remoteFileSystem = null): bool
+    {
+        if ($remoteFileSystem) {
+            return (bool) $remoteFileSystem->put($targetPath, $this->localFileSystem->get($file->getPathname()));
+        }
+
+        if ($file->isUploaded()) {
+            return move_uploaded_file($file->getPathname(), $targetPath);
+        }
+
+        return $this->localFileSystem->copy($file->getPathname(), $targetPath);
+    }
+}
